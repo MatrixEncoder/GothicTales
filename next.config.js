b4da9516081393239a60beba36b2d532c61d4551
SURYANSH SRIVASTAVA <dhawalsri77@gmail.com>
2025-02-09 22:55:56 +0530

    Setup GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,27 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        port: '',
-        pathname: '/photos/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-        port: '',
-        pathname: '/wikipedia/commons/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
+    domains: ['images.unsplash.com'],
+    unoptimized: true,
   },
+  basePath: '/GothicTales',
+  assetPrefix: '/GothicTales/',
 }
 
 module.exports = nextConfig
