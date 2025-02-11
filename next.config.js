e4b6bbb79a8fe77db1e346dd17a4e2227ceb5cb1
Gothic Tales Dev <gothictales@example.com>
2025-02-11 16:31:19 +0530

    Add GitHub Pages deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,31 +1,21 @@
-<<<<<<< HEAD
 require('dotenv').config({ path: '.env.local' });
 
-// Load environment variables
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['images.unsplash.com'],
-  },
-  env: {
-    RESEND_API_KEY: process.env.RESEND_API_KEY
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    return config;
-  }
-=======
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
-    domains: ['images.unsplash.com'],
     unoptimized: true,
+    domains: ['images.unsplash.com']
+  },
+  env: {
+    RESEND_API_KEY: process.env.RESEND_API_KEY
   },
   basePath: '/GothicTales',
   assetPrefix: '/GothicTales/',
->>>>>>> b4da9516081393239a60beba36b2d532c61d4551
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  }
 }
 
 module.exports = nextConfig
